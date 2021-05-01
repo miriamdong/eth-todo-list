cde7b778258084fc8ade2eb301a323025277029d
Miriam Dong <miriamdong@gmail.com>
2021-05-01 14:04:16 -0700

    2.List tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,15 +1,22 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
+   uint public taskCount = 0;
 
-uint public taskCount = 0;
-
-struct Task {
+   struct Task {
     uint id;
     string content;
     bool completed;
   }
 
+   mapping(uint => Task) public tasks;
 
+  constructor() public {
+    createTask("Finish the TodoList");
+  }
 
+  function createTask(string memory _content) public {
+     taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
 }
